fabe0606a789ca7e3e0e90bf3a53d2d53ac047e2
farisa <farisaap92@gmail.com>
2022-08-25 09:40:31 +0530

    Registration completed

---
+++ calc.js
@@ -0,0 +1,19 @@
+var x=10;
+var y=8;
+ export function addNumbers(x,y)
+{
+return (x+y);
+}
+
+ export function multiplication(x,y)
+  {
+    return (x*y);
+  }
+
+ function isEven(x){
+   var rem=x%2;
+return (rem==0)
+}
+var s=addNumbers(x,y);
+var m=multiplication(x,y);
+console.log(s,m);
